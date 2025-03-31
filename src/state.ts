0251579474660d8158e43cbea14a506ce76cb20b
Vien Dinh <maxvien@gmail.com>
2025-03-31 10:17:19 +0000

    ok

--- src/state.ts
+++ src/state.ts
@@ -1,15 +1,13 @@
 import { useState, useCallback } from 'react';
 
-export function useSafeState<T>(initialState: T) {
+export function useSafeState<T>(initialState: T): [T, (updater: T | ((prevState: T) => T)) => void] {
   const [state, setState] = useState<T>(structuredClone(initialState));
 
   const setStateWithClone = useCallback((updater: T | ((prevState: T) => T)) => {
     if (typeof updater === 'function') {
       setState((prevState) => {
         const updaterFn = updater as (prevState: T) => T;
-
         const nextState = updaterFn(structuredClone(prevState));
-
         return structuredClone(nextState);
       });
     } else {
